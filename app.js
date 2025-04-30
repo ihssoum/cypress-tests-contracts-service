731cc3b1494bfd849c7f4f30eebe5da25e4025e5
achrafhammi <achrafhammi29@gmail.com>
2025-04-30 08:48:54 +0100

    matched with requests with ones coming from test framework

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const contractController = require('./controllers/contractController');
 const app = express();
 
-app.get('/contract', contractController.getContract);
+app.get('/contracts', contractController.getContract);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
