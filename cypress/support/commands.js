457ccfdcc8127aa47632127b0df4dde38389d1a4
achrafhammi <achrafhammi29@gmail.com>
2025-04-30 08:47:47 +0100

    matched the framework with responses from dataprovider service

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,33 +1,20 @@
-// Cypress.Commands.add("debugg", (message, data) => {
-//     Cypress.log({
-//       name: "DEBUGG",
-//       message: message,
-//       consoleProps: () => {
-//         return { data };
-//       }
-//     });
-//     // Log to browser console as a backup
-//     console.log(message, data);
-//     return cy.wrap(data);
-//   });
-
-import { log } from "console";
-
 Cypress.Commands.add("fetchContracts", () => {
-  cy.request("http://localhost:3000/contracts").then((apiResponse) => {
-    const apiData = apiResponse.body.map((contract => {
-        return {
-            ...contract,        
-            id: Number(contract.id) 
-          };
-    }));
+  cy.request("http://localhost:3000/contracts?client=abt").then((apiResponse) => {
+    const apiData = apiResponse.body.map((contract) => {
+      return {
+        ...contract,
+        id: Number(contract.id),
+      };
+    });
 
     cy.fixture("contracts.json").then((contracts) => {
       const updatedContracts = contracts.map((contract) => {
         if (contract.queryParams) {
-          const [key, value] = Object.entries(contract.queryParams)[0];
-          const matchingData = apiData.find((item) => item[key] === value);
-
+          const matchingData = apiData.find((item) => {
+            return Object.entries(contract.queryParams).every(
+              ([key, value]) => item[key] === value
+            );
+          });
           if (matchingData) {
             contract.responseBody = matchingData;
           }
@@ -35,7 +22,6 @@ Cypress.Commands.add("fetchContracts", () => {
         return contract;
       });
 
-      // Just pass the filename, not the full path
       cy.task("writeFile", {
         filePath: "contracts.json",
         content: JSON.stringify(updatedContracts, null, 2),
