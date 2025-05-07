1fab56435bf41560da4f71b0034c5c542c9e1580
ihssoum <soumirihssane85@gmail.com>
2025-05-07 09:47:50 +0100

    modification de structure des fichiers

--- cypress/e2e/otp/01_send/01_OTP.cy.js
+++
@@ -1,8 +0,0 @@
-describe("testing fixtures", ()=>{
-    it("should update the mail in fixture", ()=>{
-        cy.fixture("example").then((data)=>{
-            data.email = "somethingssss@gmail.com"
-            //cy.writeFile("cypress/fixtures/example.json", data)
-        })
-    })
-})
--- cypress/fixtures/SELECT table_name, column_name, data_typ.sql
+++
@@ -1,4 +0,0 @@
-SELECT table_name, column_name, data_type
-FROM information_schema.columns
-WHERE table_name IN ('DISPOSITIF_VALIDATION', 'PRODUIT_SOUSCRIPTION', 'UTILISATEUR','RELATION_COMMERCIALE','COMPTE','')
-ORDER BY table_name, ordinal_position;
